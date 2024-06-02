2573d1c864314b1bf010dccc2d3e5a1c637a8d2c
Robert Grunau <robertgrunau@hey.com>
2024-06-01 18:54:03 -0700

    run threads

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from openai import OpenAI
 import os
 from dotenv import find_dotenv, load_dotenv
+from pydantic import BaseModel
+from typing import Literal
+import time
+
 load_dotenv()
 app = FastAPI()
 
@@ -26,5 +30,47 @@ async def read_root():
 
 @app.get("/assistant/create-thread")
 async def create_thread():
-    response = openai_client
-    return response
+    response = openai_client.beta.threads.create()
+    return {
+        "message": "What can I help you with today?",
+        "thread_id": response.id
+    }
+
+class MessageRequest(BaseModel):
+    message: str
+    role: Literal["user", "assistant"]
+
+@app.post("/assistant/messages/{thread_id}")
+async def send_message(thread_id: str, request: MessageRequest):
+    message = openai_client.beta.threads.messages.create(
+        thread_id=thread_id,
+        content=request.message,
+        role=request.role
+    )
+
+    run = openai_client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=os.getenv("OPEN_AI_ASSISTANT_ID") or "assistant-id",
+        instructions="Please respond to the user's message as clearly and simply as possible using the Feynman technique.",
+    )
+
+    while True:
+        run_status = openai_client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
+        print(run_status.status)
+        if run_status.status == "completed":
+            break
+        elif run_status.status == "failed":
+            return {"message": "Error: Run failed."}
+        time.sleep(1)
+    messages = openai_client.beta.threads.messages.list(thread_id=thread_id)
+    print(messages.data)
+    # Get the last messa
+    number_of_messages = len(messages.data)
+    print(number_of_messages)
+    for message in messages.data:
+        role = message.role  
+        if role == "assistant":
+            for content in message.content:
+                if content.type == "text":
+                    return {"message": content.text.value}
+            return {"message": message.content}
